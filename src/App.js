0ab2b27757753cbb089dac65a535278a20e4f6c5
productuidev <productuidev@gmail.com>
2022-06-09 00:39:09 +0900

    Styled Components Button Test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import './App.css';
 //import './App.scss';
+import styled, { css } from 'styled-components'
 import styles from "./App.module.css";
 import Button from "./components/Button";
 import StyledButton from "./components/StyledButton";
@@ -13,13 +14,37 @@ console.log(styles);
 //   logo: "App_logo__eZSaV"  
 // }
 
+const PrimaryStyledButton = styled(StyledButton)`
+  background: #f00;
+  color: #fff;
+  border-color: #f00;
+`;
+
+const UppercaseButton = (props) => <button {...props} children={props.children.toUpperCase()} />;
+const MyButton = (props) => <button className={props.className} children={`My ${props.children}`} />;
+const StyledMyButton = styled(MyButton)`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #FFF;
+  color:#FFF;
+  margin: 1.5em 1em;
+  padding: 1em 2.5em;
+  font-size:1em; 
+  display:block;
+`;
+
 function App() {
   return (
     <div className={styles["App"]}>
       <header className={styles["header"]}>
-        <p className={styles["logo"]}>React Component Styling</p>
+        {/* <p className={styles["logo"]}>React Component Styling</p> */}
         <Button>Button</Button>
-        <StyledButton>StyledButton</StyledButton>
+        <StyledButton>Button</StyledButton>
+        <StyledButton primary>Button</StyledButton>
+        <PrimaryStyledButton>Button</PrimaryStyledButton>
+        <StyledButton as="a" href="#none">Button</StyledButton>
+        <StyledButton as={UppercaseButton}>Button</StyledButton>
+        <StyledMyButton>Button</StyledMyButton>
       </header>
     </div>
   );
--- src/components/StyledButton.jsx
+++ src/components/StyledButton.jsx
@@ -1,15 +1,23 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components'
 
+// type string
 const StyledButton = styled.button`
   background: transparent;
   border-radius: 3px;
   border: 2px solid #FFF;
   color:#FFF;
-  margin: 0 1em;
+  margin: 1.5em 1em;
   padding: 1em 2.5em;
-  color:#FFF;
   font-size:1em; 
   display:block;
+
+  ${(props) =>
+    props.primary && 
+    css`
+      background: #f00;
+      color: #fff;
+      border-color: #f00;
+  `}
 `;
 
 export default StyledButton;
